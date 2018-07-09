db5d7efdabdd799384138ba48f0b6c4ebeae0809
Monica Rodrigues <monica85rodrigues@gmail.com>
2018-07-10 00:17:14 +0100

    add dictionaries benchmark

---
+++ BenchmarkDotnetDemos/BenchmarkDotnetDemos/DictionariesBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace BenchmarkDotnetDemos
+{
+    [MemoryDiagnoser]
+    public class DictionariesBenchmark
+    {
+        private IDictionary<string,string> dict = new Dictionary<string, string>();
+        private ConcurrentDictionary<string, string> concurrentDict = new ConcurrentDictionary<string, string>();
+
+        [Benchmark]
+        public IDictionary<string, string> AddDictionary()
+        {
+            dict.TryAdd("key", "value");
+            return dict;
+        }
+
+        [Benchmark]
+        public ConcurrentDictionary<string, string> AddConcurrentDictionary()
+        {
+            concurrentDict.TryAdd<string, string>("key", "value");
+            return concurrentDict;
+        }
+
+        [Benchmark]
+        public string GetValueFromDictionary()
+        {
+            dict.TryGetValue("key", out string value);
+            return value;
+        }
+
+        [Benchmark]
+        public string GetValueFromConcurrentDictionary()
+        {
+            concurrentDict.TryGetValue("key", out string value);
+            return value;
+        }
+    }
+}
